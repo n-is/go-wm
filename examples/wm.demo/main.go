dacafdf3ac44f2cdb9136ca1de61577b04b76df3
n-is <nschl.npl@gmail.com>
2020-07-26 15:33:50 +0545

    Add workspace manager example.

---
+++ examples/wm.demo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/n-is/go-wm/wm"
+)
+
+var ConsoleReader = bufio.NewReader(os.Stdin)
+
+func x(p *wm.Project) {
+	log.Println(*p)
+}
+
+func main() {
+
+	ws := wm.OpenWorkSpace("hike")
+	pr, _ := ws.AddNewProject("helloWorld", "./tmp")
+	pr.Run(x)
+	pr.Save()
+
+	fmt.Println(wm.RemoveWorkspace("hike"))
+}
+
+func prompt(str string) string {
+	fmt.Print(str)
+	text, _ := ConsoleReader.ReadString('\n')
+	return strings.TrimSpace(text[:len(text)-1])
+}
